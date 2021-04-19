1747d02e3baa44bd2aec28b7bf8c7447ddaf3905
Anders Christiansen Sørby <anders@sorby.xyz>
2021-04-19 13:55:10 +0200

    Add README documentation

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ A fresh FRAME-based [Substrate](https://www.substrate.io/) node, ready for hacki
 
 Follow these steps to get started with the Node Template :hammer_and_wrench:
 
+### Using Nix
+
+Install [nix](https://nixos.org/) and optionally [direnv](https://github.com/direnv/direnv) and [lorri](https://github.com/target/lorri) for a fully plug
+and play experience for setting up the development environment. To get all the correct dependencies activate direnv `direnv allow` and lorri `lorri shell`.
+
 ### Rust Setup
 
 First, complete the [basic Rust setup instructions](./doc/rust-setup.md).
--- nix/default.nix
+++ nix/default.nix
@@ -7,7 +7,7 @@ let
   # gitignore.nix 
   gitignoreSource = (import sources."gitignore.nix" { inherit (pkgs) lib; }).gitignoreSource;
 
-  # pre-commit-hooks = (import sources."pre-commit-hooks.nix");
+  pre-commit-hooks = (import sources."pre-commit-hooks.nix");
 
   rust = (import ./rust.nix { inherit sources; });
 
@@ -17,26 +17,26 @@ in
   inherit pkgs src;
 
   # provided by shell.nix
-  devTools = with pkgs; [
+  devTools = with pkgs; with pre-commit-hooks; [
     niv wasm-pack wasmtime valgrind protobuf clang llvm rocksdb
-    # inherit (pre-commit-hooks) pre-commit nixpkgs-fmt nix-linter;
+    pre-commit nixpkgs-fmt nix-linter
     rust
   ];
 
   # to be built by github actions
   ci = {
-    # pre-commit-check = pre-commit-hooks.run {
-    #   inherit src;
-    #   hooks = {
-    #     shellcheck.enable = true;
-    #     nixpkgs-fmt.enable = true;
-    #     nix-linter.enable = true;
-    #     # cargo-check.enable = true;
-    #     # rustfmt.enable = true;
-    #     # clippy.enable = true;
-    #   };
-    #   # generated files
-    #   excludes = [ "^nix/sources\.nix$" ];
-    # };
+    pre-commit-check = pre-commit-hooks.run {
+      inherit src;
+      hooks = {
+        shellcheck.enable = true;
+        nixpkgs-fmt.enable = true;
+        nix-linter.enable = true;
+        cargo-check.enable = true;
+        rustfmt.enable = true;
+        clippy.enable = true;
+      };
+      # generated files
+      excludes = [ "^nix/sources\.nix$" ];
+    };
   };
 }
