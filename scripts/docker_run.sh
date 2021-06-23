e54c97edd8eaff2e9eda83c1b8575d63c084c218
hamidra <hamid.alipour@gmail.com>
2021-06-23 13:15:34 -0700

    fix missing docker-compose volume source (./.local) (#189)

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -6,5 +6,7 @@ echo "*** Start Substrate node template ***"
 
 cd $(dirname ${BASH_SOURCE[0]})/..
 
+mkdir -p ./.local
+
 docker-compose down --remove-orphans
 docker-compose run --rm --service-ports dev $@
