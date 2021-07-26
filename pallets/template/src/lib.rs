a5c5fbb4bb082a9660f3a6d72345d4c38197ba4e
Sacha <alexander.lansky@gmail.com>
2021-07-26 15:46:37 +0200

    cargo fmt

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,27 +1,27 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use codec::{Decode, Encode};
 use sp_std::prelude::*;
-use codec::{Encode, Decode};
 
 // Re-export pallet items so that they can be accessed from the crate namespace.
 pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
-	use frame_system::pallet_prelude::*;
-	use sp_std::vec::Vec;
+    use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
+    use frame_system::pallet_prelude::*;
+    use sp_std::vec::Vec;
 
     #[pallet::config]
-	pub trait Config: frame_system::Config {
+    pub trait Config: frame_system::Config {
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
     }
 
     #[pallet::event]
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	#[pallet::metadata(T::AccountId = "AccountId")]
-	pub enum Event<T: Config> {
-	/// Event emitted when a proof has been claimed. [who, claim]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    #[pallet::metadata(T::AccountId = "AccountId")]
+    pub enum Event<T: Config> {
+        /// Event emitted when a proof has been claimed. [who, claim]
         ClaimCreated(T::AccountId, Vec<u8>),
         /// Event emitted when a claim is revoked by the owner. [who, claim]
         ClaimRevoked(T::AccountId, Vec<u8>),
@@ -36,16 +36,17 @@ pub mod pallet {
         /// The proof is claimed by another account, so caller can't revoke it.
         NotProofOwner,
     }
-    
+
     #[pallet::pallet]
     #[pallet::generate_store(pub(super) trait Store)]
     pub struct Pallet<T>(_);
 
     #[pallet::storage]
-    pub(super) type Proofs<T: Config> = StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber), ValueQuery>;   
-    
+    pub(super) type Proofs<T: Config> =
+        StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber), ValueQuery>;
+
     #[pallet::hooks]
-	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
@@ -54,21 +55,22 @@ pub mod pallet {
             origin: OriginFor<T>,
             proof: Vec<u8>,
         ) -> DispatchResultWithPostInfo {
-
             // Check that the extrinsic was signed and get the signer.
             // This function will return an error if the extrinsic is not signed.
             // https://substrate.dev/docs/en/knowledgebase/runtime/origin
             let sender = ensure_signed(origin)?;
-           
-            // Verify that the specified proof has not already been claimed.         
-            ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
+
+            // Verify that the specified proof has not already been claimed.
+            ensure!(
+                !Proofs::<T>::contains_key(&proof),
+                Error::<T>::ProofAlreadyClaimed
+            );
 
             // Get the block number from the FRAME System module.
             let current_block = <frame_system::Module<T>>::block_number();
 
             // Store the proof with the sender and block number.
-           Proofs::<T>::insert(&proof, (&sender, current_block));
-           
+            Proofs::<T>::insert(&proof, (&sender, current_block));
 
             // Emit an event that the claim was created.
             Self::deposit_event(Event::ClaimCreated(sender, proof));
@@ -77,10 +79,7 @@ pub mod pallet {
         }
 
         #[pallet::weight(10_000)]
-        fn revoke_claim(
-            origin: OriginFor<T>,
-            proof: Vec<u8>,
-        ) -> DispatchResultWithPostInfo {
+        fn revoke_claim(origin: OriginFor<T>, proof: Vec<u8>) -> DispatchResultWithPostInfo {
             // Check that the extrinsic was signed and get the signer.
             // This function will return an error if the extrinsic is not signed.
             // https://substrate.dev/docs/en/knowledgebase/runtime/origin
