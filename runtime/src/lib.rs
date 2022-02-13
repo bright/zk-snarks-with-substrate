d9944b6eb4ce430dcf155137bcb0cb7922e1f7bd
Shawn Tabrizi <shawntabrizi@gmail.com>
2022-02-13 07:12:51 -0700

    fix node compile

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -25,7 +25,9 @@ use sp_version::RuntimeVersion;
 
 // A few exports that help ease life for downstream crates.
 pub use frame_support::{
-	construct_runtime, parameter_types,
+	construct_runtime,
+	pallet_prelude::ConstU32,
+	parameter_types,
 	traits::{KeyOwnerProofSystem, Randomness, StorageInfo},
 	weights::{
 		constants::{BlockExecutionWeight, ExtrinsicBaseWeight, RocksDbWeight, WEIGHT_PER_SECOND},
